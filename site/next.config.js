e0ec45640f2d3fb0375c2ee3f89cc840d064ab18
Luigi Serra <luigser@gmail.com>
2024-05-07 12:43:03 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,8 +2,8 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'DI Kasm Registry',
+    description: 'The DI workspaces store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
     listUrl: 'https://registry.kasmweb.com/',
     contactUrl: 'https://kasmweb.com/support',
